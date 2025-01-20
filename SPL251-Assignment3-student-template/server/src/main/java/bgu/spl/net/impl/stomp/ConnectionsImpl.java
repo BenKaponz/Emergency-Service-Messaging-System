701954e882997f45b5afa83e626e07a1ec93087b
Itay Shaul <itayshaul111@gmail.com>
2025-01-20 18:02:14 +0200

    Updated ConnectionsImpl functions
    Version 1

--- SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -1,29 +1,50 @@
 package bgu.spl.net.impl.stomp;
 
-import java.sql.Connection;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
+import bgu.spl.net.srv.ConnectionHandler;
 import bgu.spl.net.srv.Connections;
 
 public class ConnectionsImpl<T> implements Connections<T> {
 
+    private final ConcurrentHashMap<Integer, ConnectionHandler<T>> activeConnections;
+    
+    private final ConcurrentHashMap<String, List<Integer>> channelSubscriptions;
+
+    public ConnectionsImpl() {
+        this.activeConnections = new ConcurrentHashMap<>();
+        this.channelSubscriptions = new ConcurrentHashMap<>();
+    }
+
     @Override
     public boolean send(int connectionId, T msg) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'send'");
+        ConnectionHandler<T> handler = activeConnections.get(connectionId);
+        if (handler != null) {
+            handler.send(msg);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void send(String channel, T msg) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'send'");
+        List<Integer> subscribers = channelSubscriptions.get(channel);
+        if (subscribers != null) {
+            for (int connectionId : subscribers) {
+                send(connectionId, msg);
+            }
+        }
     }
 
     @Override
     public void disconnect(int connectionId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'disconnect'");
+        activeConnections.remove(connectionId);
+        for (List<Integer> subscribers : channelSubscriptions.values()) {
+            subscribers.remove(Integer.valueOf(connectionId));
+        }
     }
 
-    
-    
+
+
 }
