43996833d6a2562a62771a2a1aae3dabc458c13c
BenKaponz <benkapon0560@gmail.com>
2025-01-20 17:34:48 +0200

    Update ConnectionsImpl.java

---
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -0,0 +1,29 @@
+package bgu.spl.net.impl.stomp;
+
+import java.sql.Connection;
+
+import bgu.spl.net.srv.Connections;
+
+public class ConnectionsImpl<T> implements Connections<T> {
+
+    @Override
+    public boolean send(int connectionId, T msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'send'");
+    }
+
+    @Override
+    public void send(String channel, T msg) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'send'");
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'disconnect'");
+    }
+
+    
+    
+}
