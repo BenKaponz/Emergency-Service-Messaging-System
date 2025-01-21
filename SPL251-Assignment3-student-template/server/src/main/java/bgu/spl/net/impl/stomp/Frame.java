48e6d95d49f1693516868852817c399904e2e4f8
BenKaponz <benkapon0560@gmail.com>
2025-01-21 21:20:30 +0200

    Update Frame.java

--- SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/Frame.java
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/Frame.java
@@ -1,47 +1,49 @@
-package bgu.spl.net.impl.stomp;
+// package bgu.spl.net.impl.stomp;
 
-import java.util.LinkedList;
-import java.util.List;
+// import java.util.LinkedList;
+// import java.util.List;
 
-public class Frame {
+// public class Frame {
 
-    private String command;
-    private List<String> headers; //HeaderType & Value
-    private String body;
-    
-    public Frame(String message) {
-        this.headers = new LinkedList<>();
+//     private String command;
+//     private List<String> headers; // HeaderType & Value
+//     private List<String> body;
 
-        // Spliting msg 
-        String[] msgLines = message.split("\n");
-    
-        // First line is the command
-        command = msgLines[0]; 
-        int index;
+//     public Frame(String message) {
+//         this.headers = new LinkedList<>();
 
-        for (index = 1 ;index < msgLines.length; index++) {
-            // Break when finished headers.
-            if (msgLines[index].equals("")){
-                break;
-            }
-            headers.add(msgLines[index]);
-        }
-        
-        // After the empty line comes the body.
-        index++;
-        this.body = msgLines[index];
-    }
+//         // Spliting msg
+//         String[] msgLines = message.split("\n");
 
-    public String getCommand() {
-        return command;
-    }
+//         // First line is the command
+//         command = msgLines[0];
+//         int index;
 
-    public List<String> getHeaders() {
-        return headers;
-    }
+//         for (index = 1; index < msgLines.length; index++) {
+//             // Break when finished headers.
+//             if (msgLines[index].equals("")) {
+//                 // After the empty line comes the body.
+//                 index++;
+//                 break;
+//             }
+//             headers.add(msgLines[index]);
+//         }
 
-    public String getBody() {
-        return body;
-    }
+//         while (index < msgLines.length) {
+//             body.add(msgLines[index]);
+//         }
+//     }
 
-}
+//     public String getCommand() {
+//         return command;
+//     }
+
+//     public List<String> getHeaders() {
+//         return headers;
+//     }
+
+//     public List<String> getBody() {
+//         return body;
+//     }
+
+// }
