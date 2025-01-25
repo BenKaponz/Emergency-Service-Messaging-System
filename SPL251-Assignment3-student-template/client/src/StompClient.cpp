6dce7b5cf8a923270ff9e10b1575479aef6c9fcc
BenKaponz <benkapon0560@gmail.com>
2025-01-25 18:47:18 +0200

    Updated client main

--- SPL251-Assignment3-student-template/client/src/StompClient.cpp
+++ SPL251-Assignment3-student-template/client/src/StompClient.cpp
@@ -1,4 +1,17 @@
-int main(int argc, char *argv[]) {
-	// TODO: implement the STOMP client
-	return 0;
+#include "StompProtocol.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    try {
+        ConnectionHandler* connectionHandler = nullptr;
+        StompProtocol StompProtocol(connectionHandler);
+        StompProtocol.initiate();
+    } catch (const exception& exception) {
+        cout << "Error: " << exception.what() << endl;
+        return 1;
+    }
+
+    return 0;
 }
